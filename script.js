b5b7fdd303721a1cee724057bf36fa6374166c44
arthursvpb <arthurandroid15@gmail.com>
2019-05-25 19:48:52 -0300

    funcao para transformar para binario e fazer aparecer no display

---
+++ script.js
@@ -0,0 +1,31 @@
+// FUNÇÃO PARA TRANSFORMAR OS INPUTS EM BINÁRIO
+var form = document.querySelector("form");
+var entrada1 = document.querySelector("#entrada1");
+var entrada2 = document.querySelector("#entrada2");
+var displayBin1 = document.querySelector("#displayBin1");
+var displayBin2 = document.querySelector("#displayBin2");
+
+// Converte e mostra o display do INPUT1
+entrada1.addEventListener("input", function() {
+  var bin1 = (+entrada1.value).toString(2);
+  displayBin1.textContent = bin1;
+});
+
+// Converte e mostra o display do INPUT2
+entrada2.addEventListener("input", function() {
+  var bin2 = (+entrada2.value).toString(2);
+  displayBin2.textContent = bin2;
+});
+
+// Pega os binários e salva
+form.addEventListener("submit", function (evento){
+
+  evento.preventDefault();
+
+  var bin1 = (+entrada1.value).toString(2);
+  var bin2 = (+entrada2.value).toString(2);
+
+  alert(bin1);
+  alert(bin2);
+
+});
