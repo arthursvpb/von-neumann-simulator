654c9d15576ca448a64481f8edb279035fcd8f86
arthursvpb <arthurandroid15@gmail.com>
2019-06-01 11:50:26 -0300

    funcao de pegar cada elemento na ram separadamente

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ var selectFuncao = document.querySelector("#selectFuncao");
 var selectRegister = document.querySelector("#selectRegister");
 var regDisplay = document.querySelectorAll(".regDisplay");
 var execute = document.querySelector("#execute");
+
+// Variáveis da memória RAM
 var ramValues = [];
+var functionValues = [00, 03, 06, 09, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42];
+var valueValues = [01, 04, 07, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43];
+var regValues = [02, 05, 08, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44];
 
 // Função para pegar os valores dentro da RAM
 function GetCellValues() {
